c091407ecd0f0f2860570dda78d34a91a25298e6
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-08 10:49:05 +0200

    Getters y Setters, y primer objeto persona de prueba

--- src/Persona.java
+++ src/Persona.java
@@ -18,4 +18,48 @@ public class Persona {
         this.tel = tel;
         this.email = email;
     }
+    //Getters and Setters
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getTel(){
+        return this.tel;
+    }
+    public void setTel(String tel){
+        this.tel = tel;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String emil){
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args){
+        Persona persona1 = new Persona("Juan Perez", "55443322", "juan@curso.com");
+        System.out.println(persona1);
+    }
 }
+
