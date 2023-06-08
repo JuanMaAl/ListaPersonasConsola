d2420204e459d0421bbd25dd6ce0372e8065448e
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-08 07:46:35 +0200

    Constructor vacío y Constructor con argumentos de la clase Persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,7 +1,21 @@
 public class Persona {
+    //Atributos
     private int id;
     private String nombre;
     private String tel;
     private String email;
     private static int numeroPersonas = 0;
+
+    //Constructor vacío
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    //Constructor con argumentos
+    public Persona(String nombre, String tel, String email){
+        this(); //Se llama al constructor vacío
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
 }
