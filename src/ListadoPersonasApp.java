114e34c8bf010e2ebbf800be79851e3ae5338513
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-08 12:35:26 +0200

    Scanner, Lista<Persona>, estrutura ciclo while que envuelve la parte principal de la app

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -4,8 +4,19 @@
     FullStack con Angular, React y Spring Boot!
     https://www.udemy.com/course/aprende-java-en-13-dias-con-aplicaciones-del-mundo-real-spring/
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class ListadoPersonasApp {
     public static void main(String[] args) {
+    Scanner consola = new Scanner(System.in);
+    //Definimos la lista fuera del ciclo while
+    List<Persona> personas = new ArrayList<>();
+    //Empezamos con el menú
+    var salir = false;
+    while(!false){
+        
+    }
 
     } //fin main
 } // fin clase Main
